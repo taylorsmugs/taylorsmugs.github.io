ecf0f8a75ff3f575e409d954bf3a67c685197667
tfrasc <tfrasc@gmail.com>
2016-01-08 23:48:39 -0800

    a little more

--- script.js
+++ script.js
@@ -5,8 +5,30 @@ $(document).ready(function() {
 //		$(this).toggleClass("lowlight");
 	});
 	
-	$('.tab').click(function() {
-		$('.tab').removeClass("highlight")
-		$(this).addClass("highlight")
-	});
+//	$('.tab').click(function() {
+	//	$('.tab').removeClass("highlight")
+//		$(this).addClass("highlight"),
+//    $('.block').html(
+ //     <iframe src="orders.html" width="200" height="200"></iframe>)
+//	});
+
+  $('.tab').click(function() {
+    $('.tab').removeClass('highlight');
+    $(this).addClass('highlight');
+    switch( true ){
+     case $(this).is('.about'):
+       $('.block').load('about.html');
+       break;
+     case $(this).is('.instagram'):
+        $('.block').load('instagram.html');
+        break;
+      case $(this).is('.orders'):
+        $('.block').load('orders.html');
+        break;
+     case $(this).is('about'):
+        $('.block').load('about.html');
+        break;
+      }
+  });
+
 });
